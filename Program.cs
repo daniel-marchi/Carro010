6fb3e68c5605019c3514a94d971b7964f412e9cc
Daniel Marchi <marchimail@gmail.com>
2020-06-16 09:44:32 -0300

    meu primeiro commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Carros010
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var carro = new Carro();
+
+            carro.Ligar();
+        }
+    }
+
+    public class Carro
+    {
+        //Propriedades = características
+        //Métodos = ações
+
+        public void Ligar()
+        {
+            Console.WriteLine("Carro Ligado");
+        }
+    }
+}
